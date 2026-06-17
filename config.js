a4f5dfbefc4658b13d062981467c97605f689d54
pwfgb9k2kb-lang <pwfgb9k2kb@privaterelay.appleid.com>
2026-06-17 14:37:50 -0300

    Clear CLAUDE_API_KEY in config.js
    Removed sensitive CLAUDE_API_KEY from configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  CLAUDE_API_KEY: "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",
+  CLAUDE_API_KEY:"",
   PROXY_URL: "https://wild-pond-b15e.nico-boulanger2004.workers.dev",
   EMPRESA: "Sulfoquímica SA",
   SECRET_PATH: "contabilidad-xk7m2p",
